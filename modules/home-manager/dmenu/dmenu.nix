e4a29ddfdcbfbaea79dc7d37d715e58e87881a30
Kaddu720 <nwilsonmalgus@gmail.com>
2024-04-25 23:45:18 -0700

    patch dmenu for rose pine

--- modules/home-manager/dmenu/dmenu.nix
+++ modules/home-manager/dmenu/dmenu.nix
@@ -8,7 +8,7 @@
         home.packages = let 
             dmenu = pkgs.dmenu.overrideAttrs (old: {
                 # my own patched version of dmenu
-                src = ../dmenu-5.3.tar.gz;
+                src = ./dmenu-5.3.tar.gz;
             });
         in with pkgs; [
             dmenu
---
+++ modules/home-manager/dmenu/source/colors-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e0def4", "#191724" },
+	[SchemeSel] = { "#191724", "#ebbcba" },
+	[SchemeOut] = { "#e0def4", "#403d52" },
+};
