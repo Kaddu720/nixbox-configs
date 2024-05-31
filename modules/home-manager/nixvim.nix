a6b10848a39315ebbfa2a55ad77f42316c9826a5
Kaddu720 <nwilsonmalgus@gmail.com>
2024-05-31 15:08:48 -0700

    Configure Harpoon

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -13,6 +13,7 @@
 
             plugins = {
                 cmp.enable = true;
+                cmp-treesitter.enable =  true;
                 fugitive.enable = true;
                 harpoon.enable = true;
                 lualine.enable = true;
@@ -63,6 +64,7 @@
 
             globals.mapleader = " ";
             keymaps = [
+                # Telescope
                 {
                     mode = "n";
                     key = "<leader>ff";
@@ -84,6 +86,50 @@
                     options.desc = "Telescop find words";
                 }
 
+                # Harpoon
+                {
+                    mode = "n";
+                    key = "<leader>hm";
+                    action = "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>";
+                    options.desc = "Harpoon Quick Menu";
+                }
+
+                {
+                    mode = "n";
+                    key = "<leader>ha";
+                    action = "<cmd>lua require('harpoon.mark').add_file()<CR>";
+                    options.desc = "Add a file to Harpoon";
+                }
+
+                {
+                    mode = "n";
+                    key = "<leader>h1";
+                    action = "<cmd>lua require('harpoon.ui').nav_file(1)<CR>";
+                    options.desc = "Navigate to file 1";
+                }
+
+                {
+                    mode = "n";
+                    key = "<leader>h2";
+                    action = "<cmd>lua require('harpoon.ui').nav_file(2)<CR>";
+                    options.desc = "Navigate to file 2";
+                }
+
+                {
+                    mode = "n";
+                    key = "<leader>h3";
+                    action = "<cmd>lua require('harpoon.ui').nav_file(3)<CR>";
+                    options.desc = "Navigate to file 3";
+                }
+
+                {
+                    mode = "n";
+                    key = "<leader>h4";
+                    action = "<cmd>lua require('harpoon.ui').nav_file(4)<CR>";
+                    options.desc = "Navigate to file 4";
+                }
+
+                # Nvim Tree
                 {
                     mode = "n";
                     key = "<leader>ee";
