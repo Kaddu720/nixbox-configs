92afd69bf971cc71e5682dfd844e5387c74c32ce
Kaddu720 <nwilsonmalgus@gmail.com>
2024-06-06 18:35:41 -0700

    Clea up flake.nix

--- flake.nix
+++ flake.nix
@@ -15,53 +15,46 @@
         };
 
 
-	nix-darwin = {
-	    url = "github:LnL7/nix-darwin";
-	    inputs.nixpkgs.follows = "nixpkgs";
-	};
-
-        nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    };
-
-    outputs = { self, nixpkgs, nixos-hardware, nix-darwin, ... }@inputs:
-    	let
-	    nixpkgsConfig = {
-	        config.allowUnfree = true;
+        darwin = {
+	        url = "github:LnL7/nix-darwin";
+	        inputs.nixpkgs.follows = "nixpkgs";
 	    };
 
-	in
-        {
-            nixosConfigurations = {
-                Home-Box = nixpkgs.lib.nixosSystem {
-		    system = "x86-64-linux";
-                    specialArgs = { inherit inputs; };
-                    modules = [ 
-                        ./hosts/home-box/configuration.nix 
-                        inputs.home-manager.nixosModules.default
-                        inputs.nixvim.nixosModules.nixvim
-                    ];
-                };
-                Mobile-Box = nixpkgs.lib.nixosSystem {
-		    system = "x86-64-linux";
-                    specialArgs = { inherit inputs; };
-                    modules = [ 
-                        ./hosts/mobile-box/configuration.nix 
-                        inputs.home-manager.nixosModules.default
-                        inputs.nixvim.nixosModules.nixvim
-                        inputs.nixos-hardware.nixosModules.framework-12th-gen-intel
-                    ];
-                };
-	  };
-	  darwinConfigurations = {
-		Work-Box = nix-darwin.lib.darwinSystem {
-		    system = "aarch64-darwin";
-		    specialArgs = { inherit inputs; };
-		    modules = [
-		    	./hosts/work-box/configuration.nix
-			inputs.home-manager.darwinModules.home-manager
-		    ];
-		};
-            };
-
+        nixos-hardware = {
+            url = "github:NixOS/nixos-hardware/master";
+            inpusts.nixpkgs.follows = "nixpkgs";
         };
+    };
+
+    outputs = { nixpkgs, darwin, home-manager, nixos-hardware, ... }@inputs: {
+        nixosConfigurations = {
+            Home-Box = nixpkgs.lib.nixosSystem {
+                system = "x86-64-linux";
+                specialArgs = { inherit inputs; };
+                modules = [ 
+                    ./hosts/home-box/configuration.nix 
+                    home-manager.nixosModules.default
+                ];
+            };
+            Mobile-Box = nixpkgs.lib.nixosSystem {
+                system = "x86-64-linux";
+                specialArgs = { inherit inputs; };
+                modules = [ 
+                    ./hosts/mobile-box/configuration.nix 
+                    inputs.home-manager.nixosModules.default
+                    nixos-hardware.nixosModules.framework-12th-gen-intel
+                ];
+            };
+        };
+        darwinConfigurations = {
+            Work-Box = darwin.lib.darwinSystem {
+                system = "aarch64-darwin";
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./hosts/work-box/configuration.nix
+                    home-manager.darwinModules.home-manager
+                ];
+            };
+        };
+    };
 }
