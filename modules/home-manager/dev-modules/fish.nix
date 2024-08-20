566d069c267787501ce99e3d33793468a522a4f1
Kaddu720 <nwilsonmalgus@gmail.com>
2024-08-20 12:54:18 -0700

    update wifi script

--- modules/home-manager/dev-modules/fish.nix
+++ modules/home-manager/dev-modules/fish.nix
@@ -36,8 +36,9 @@
                 #Wifi Function
                 function dmenu-wifi
                     set bssid $(nmcli device wifi list | sed -n '1!p' | cut -b 9- | dmenu -p "Select wifi" -l 20  | cut -d ' ' -f1)
+                    set name $(echo "" 	| dmenu -p "Enter Username : ")
                     set pass $(echo "" 	| dmenu -p "Enter Password : ")
-                    nmcli device wifi connect $bssid password $pass 
+                    nmcli device wifi connect $bssid name $name password $pass 
                     pkill -RTMIN+9 dwmblocks
                 end
 
