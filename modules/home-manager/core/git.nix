cf3b9b94298e23a8bd54c5b5bd668de36baa5797
Kaddu720 <nwilsonmalgus@gmail.com>
2024-09-03 12:27:02 -0700

    fix git and polybar

--- modules/home-manager/core/git.nix
+++ modules/home-manager/core/git.nix
@@ -1,19 +1,22 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    git.enable =
+      lib.mkEnableOption "enables git";
+  };
 
-{ pkgs, lib, config, ... }: {
-    options = {
-        git.enable = 
-            lib.mkEnableOption "enables git";
+  config = lib.mkIf config.git.enable {
+    programs.git = {
+      enable = true;
+      userName = "Kaddu720";
+      userEmail = "nwilsonmalgus@gmail.com";
+      extraConfig = {
+        pull.rebase = true;
+        core.editor = "nvim";
+      };
     };
-
-    config = lib.mkIf config.git.enable {    
-        programs.git = {
-            enable = true;
-            userName = "Kaddu720";
-            userEmail = "nwilsonmalgus@gmail.com";
-            extraConfig = {
-                pull.rebase = true;
-            };
-        };
-    };
+  };
 }
-
