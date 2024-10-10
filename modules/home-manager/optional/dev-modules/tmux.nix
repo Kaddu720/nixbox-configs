b890fe2607502a9b3a5a561c65ddbed221bb7a17
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-10-10 15:51:37 -0700

    trying to fix tmux shell issues

--- modules/home-manager/optional/dev-modules/tmux.nix
+++ modules/home-manager/optional/dev-modules/tmux.nix
@@ -1,75 +1,81 @@
-{ pkgs, lib, config, ... }: {
-    options = {
-        tmux.enable = 
-            lib.mkEnableOption "enables tmux";
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    tmux.enable =
+      lib.mkEnableOption "enables tmux";
+  };
+
+  config = lib.mkIf config.tmux.enable {
+    programs.tmux = {
+      enable = true;
+
+      #shell = if "${config.home.username}" == "noahwilson" then "/usr/local/bin/fish" else "/home/noah/.nix-profile/bin/fish";
+      shell = "${pkgs.fish}/bin/fish";
+      mouse = true;
+      keyMode = "vi";
+      baseIndex = 1; #window and panes #s start on 1
+      terminal = "alacritty";
+
+      plugins = with pkgs.tmuxPlugins; [
+        vim-tmux-navigator
+        yank
+        rose-pine
+      ];
+      extraConfig = ''
+        #True color settings
+        set -g default-terminal "$TERM"
+        set -ag terminal-overrides ",$TERM:Tc"
+
+        #Start Windows and panes at 1, not 0
+        set-option -g renumber-windows on
+
+        #Status bar
+        set-option -g status-position top
+        set -g status-justify left
+        set -g status-style 'fg=colour1'
+        set -g status-left-length 10
+
+        setw -g window-status-current-style 'fg=colour0 bg=colour1 bold'
+        setw -g window-status-current-format ' #I #W #F '
+
+        setw -g window-status-style 'fg=colour1 dim'
+        setw -g window-status-format ' #I #[fg=colour7]#W #[fg=colour1]#F '
+
+        setw -g window-status-bell-style 'fg=colour2 bg=colour1 bold'
+
+        #Key bindings#
+        # prefix key ix now C-b
+        unbind-key C-b
+        set-option -g prefix C-a
+        bind-key C-a send-prefix
+
+        # navigate panes with vim keyes
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+
+        #enable true vi mode for copying
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+        #open panes in current directory
+        bind | split-window -h -c "#{pane_current_path}"
+        bind - split-window -v -c "#{pane_current_path}"
+        unbind '"'
+        unbind %
+
+        #adjust pane size with capital 'L" or "J"
+        bind -n M-H resize-pane -L 5
+        bind -n M-L resize-pane -R 5
+        bind -n M-K resize-pane -U 5
+        bind -n M-J resize-pane -D 5
+      '';
     };
-
-    config = lib.mkIf config.tmux.enable {    
-        programs.tmux = {
-            enable = true;
-            
-            shell = if "${config.home.username}" == "noahwilson" then "/usr/local/bin/fish" else "/home/noah/.nix-profile/bin/fish";
-            mouse = true;
-            keyMode = "vi";
-            baseIndex = 1; #window and panes #s start on 1
-            terminal = "alacritty";
-
-            plugins = with pkgs.tmuxPlugins; [
-                vim-tmux-navigator
-                yank
-                rose-pine
-            ];
-            extraConfig = ''
-                #True color settings
-                set -g default-terminal "$TERM"
-                set -ag terminal-overrides ",$TERM:Tc"
-
-                #Start Windows and panes at 1, not 0
-                set-option -g renumber-windows on
-
-                #Status bar
-                set-option -g status-position top
-                set -g status-justify left
-                set -g status-style 'fg=colour1'
-                set -g status-left-length 10
-
-                setw -g window-status-current-style 'fg=colour0 bg=colour1 bold'
-                setw -g window-status-current-format ' #I #W #F '
-
-                setw -g window-status-style 'fg=colour1 dim'
-                setw -g window-status-format ' #I #[fg=colour7]#W #[fg=colour1]#F '
-
-                setw -g window-status-bell-style 'fg=colour2 bg=colour1 bold'
-
-                #Key bindings#
-                # prefix key ix now C-b
-                unbind-key C-b
-                set-option -g prefix C-a
-                bind-key C-a send-prefix
-
-                # navigate panes with vim keyes
-                bind-key h select-pane -L
-                bind-key j select-pane -D
-                bind-key k select-pane -U
-                bind-key l select-pane -R
-
-                #enable true vi mode for copying
-                bind-key -T copy-mode-vi v send-keys -X begin-selection
-                bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-                bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-                #open panes in current directory
-                bind | split-window -h -c "#{pane_current_path}"
-                bind - split-window -v -c "#{pane_current_path}"
-                unbind '"'
-                unbind %
-
-                #adjust pane size with capital 'L" or "J"
-                bind -n M-H resize-pane -L 5
-                bind -n M-L resize-pane -R 5
-                bind -n M-K resize-pane -U 5
-                bind -n M-J resize-pane -D 5
-            '';
-        };
-    };
+  };
 }
