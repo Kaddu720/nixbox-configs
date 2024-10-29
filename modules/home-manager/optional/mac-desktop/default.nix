e10ce172a6e8cb8d953f4cbf0a8f5ec7e6b6887a
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-10-29 10:26:48 -0700

    moving homebrew back to it's right place

--- modules/home-manager/optional/mac-desktop/default.nix
+++ modules/home-manager/optional/mac-desktop/default.nix
@@ -8,14 +8,4 @@
     lib.mkDefault true;
   skhd.enable =
     lib.mkDefault true;
-
-  #enable programs as services
-  services = {
-    skhd.enable = true;
-  };
-
-  homebrew.casks = [
-    "nikitabobko/tap/aerospace"
-    "hiddenbar"
-  ];
 }
