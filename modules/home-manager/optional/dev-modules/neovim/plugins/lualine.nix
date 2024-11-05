9db76116f5c47b6ead7a1f01510f2fbc6e2c38c4
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-11-05 15:39:10 -0800

    make lualine transparent

--- modules/home-manager/optional/dev-modules/neovim/plugins/lualine.nix
+++ modules/home-manager/optional/dev-modules/neovim/plugins/lualine.nix
@@ -1,7 +1,17 @@
 {pkgs, ...}: {
   programs.nixvim = {
+    # make lualine transpraent for rose pine
+    extraConfigLua = ''
+      local custom_rosepine = require('lualine.themes.rose-pine')
+      custom_rosepine.normal.c.bg = 'None'
+      custom_rosepine.insert.c.bg = 'None'
+      custom_rosepine.command.c.bg = 'None'
+      require('lualine').setup { options = { theme = custom_rosepine}}
+    '';
     plugins = {
-      lualine.enable = true;
+      lualine = {
+        enable = true;
+      };
 
       lazy.plugins = [
         {
