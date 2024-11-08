a99b317a8d6a59b8180882346f44c897d9f87cc5
Kaddu720 <nwilsonmalgus@gmail.com>
2024-11-07 22:35:08 -0800

    remove homerow mods

--- modules/nixos/optional/kanata.nix
+++ modules/nixos/optional/kanata.nix
@@ -24,7 +24,7 @@
           config = ''
             ;; defsrc is still necessary
             (defsrc
-              lctl a s d f j k l ;
+              lctl
             )
 
             (defvar
@@ -34,18 +34,10 @@
 
             (defalias
               escctrl (tap-hold 100 100 esc lctl)
-              a (tap-hold $tap-time $hold-time a lmet)
-              s (tap-hold $tap-time $hold-time s lctl)
-              d (tap-hold $tap-time $hold-time d lsft)
-              f (tap-hold $tap-time $hold-time f lalt)
-              j (tap-hold $tap-time $hold-time j rctl)
-              k (tap-hold $tap-time $hold-time k rsft)
-              l (tap-hold $tap-time $hold-time l ralt)
-              ; (tap-hold $tap-time $hold-time ; rmet)
             )
 
             (deflayer base
-              @escctrl @a @s @d @f @j @k @l @;
+              @escctrl
             )
           '';
         };
@@ -71,20 +63,12 @@
 
             (defalias
               escctrl (tap-hold 100 100 esc lctl)
-              a (tap-hold $tap-time $hold-time a lmet)
-              s (tap-hold $tap-time $hold-time s lalt)
-              d (tap-hold $tap-time $hold-time d lsft)
-              f (tap-hold $tap-time $hold-time f lctl)
-              j (tap-hold $tap-time $hold-time j rctl)
-              k (tap-hold $tap-time $hold-time k rsft)
-              l (tap-hold $tap-time $hold-time l ralt)
-              ; (tap-hold $tap-time $hold-time ; rmet)
             )
 
             (deflayer base
               grv       1    2    3    4    5    6    7    8    9    0    -    =    \
               tab       q    w    e    r    t    y    u    i    o    p    [    ]    bspc
-              @escctrl @a   @s   @d   @f    g    h   @j   @k   @l   @;    '    ret
+              @escctrl  a    s    d    f    g    h    j    k    l    ;    '    ret
               lsft      z    x    c    v    b    n    m    ,    .    /    rsft
               lctl lmet lalt           spc            ralt rctl
             )
