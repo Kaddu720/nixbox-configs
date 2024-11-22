aa231b126fe99721e37f37eae12566b53c075673
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-11-21 16:03:23 -0800

    Trouble key bindings

--- modules/home-manager/optional/dev-modules/neovim/plugins/trouble.nix
+++ modules/home-manager/optional/dev-modules/neovim/plugins/trouble.nix
@@ -4,6 +4,10 @@
     plugins = {
       trouble = {
         enable = true;
+        settings = {
+          auto_close = true;
+          auto_jumpt = true;
+        };
       };
 
       lazy.plugins = [
@@ -13,5 +17,23 @@
         }
       ];
     };
+
+    keymaps = [
+      # Toggle Trouble
+      {
+        mode = "n";
+        key = "<leader>tt";
+        action = "<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>";
+        options.desc = "Diagnostics (Trouble)";
+      }
+
+      # Toogle Focus
+      {
+        mode = "n";
+        key = "<leader>tf";
+        action = "<cmd>Trouble diagnostics focus=true<cr>";
+        options.desc = "Focus on Diagnostics (Trouble)";
+      }
+    ];
   };
 }
