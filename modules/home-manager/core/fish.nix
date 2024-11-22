df7215cc5d43860050515c92eac438115511d9cc
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-11-21 19:22:02 -0800

    remove helper functions from fish shell

--- modules/home-manager/core/fish.nix
+++ modules/home-manager/core/fish.nix
@@ -27,36 +27,11 @@
           # Command to fix auto complete in vi mode
           bind -M insert \cf accept-autosuggestion
 
-          #enable fzf, the fuck, and zoxide
+          # enable fzf, the fuck, and zoxide
           fzf --fish | source
           eval "$(thefuck --alias)"
           eval "$(zoxide init fish)"
           alias cd "z"
-
-          # Fucntions
-          #Wifi Function
-          function rofi-wifi
-              set bssid $(nmcli device wifi list | sed -n '1!p' | cut -b 9- | rofi -dmenu -p "Select wifi" -l 20  | cut -d ' ' -f1)
-              set name $(echo "" 	| rofi -dmenu -p "Enter Username : ")
-              set pass $(echo "" 	| rofi -dmenu -p "Enter Password : ")
-              nmcli device wifi connect $bssid password $pass
-              pkill -RTMIN+9 dwmblocks
-          end
-
-          function rofi-wifi-username
-              set bssid $(nmcli device wifi list | sed -n '1!p' | cut -b 9- | rofi -dmenu -p "Select wifi" -l 20  | cut -d ' ' -f1)
-              set name $(echo "" 	| rofi -dmenu -p "Enter Username : ")
-              set pass $(echo "" 	| rofi -dmenu -p "Enter Password : ")
-              nmcli device wifi connect $bssid name $name password $pass
-              pkill -RTMIN+9 dwmblocks
-          end
-
-          #Pipe wire sinck selector
-          function rofi-sound
-              set sink $( wpctl status -k | grep -m 1 'Sinks:' --no-group-separator -A2 | grep -v 'Sinks' | cut -b 7-30 | rofi -dmenu -p "selct Audio Output" -l 2 | cut -b '5-6' )
-              wpctl set-default $sink
-              pkill -RTMIN+10 dwmblocks
-          end
         '';
     };
 
