7d0f3c4aaf9feb6b6bd7aa9bb98bb28c9303e936
Kaddu720 <nwilsonmalgus@gmail.com>
2024-11-22 01:45:38 -0800

    add tmuxifier

--- modules/home-manager/optional/dev-modules/tmux.nix
+++ modules/home-manager/optional/dev-modules/tmux.nix
@@ -10,6 +10,8 @@
   };
 
   config = lib.mkIf config.tmux.enable {
+    home.packages = with pkgs; [tmuxifier];
+
     programs.tmux = {
       enable = true;
 
@@ -77,6 +79,77 @@
         bind -n M-K resize-pane -U 5
         bind -n M-J resize-pane -D 5
       '';
+    };
+
+    home.file = {
+      ".tmux-layouts/dashboard.session.sh".text =
+        /*
+        bash
+        */
+        ''
+          # Set a custom session root path. Default is `$HOME`.
+          # Must be called before `initialize_session`.
+          session_root "~/Projects/dashboard/dev/ekiree_dashboard"
+
+          # Create session with specified name if it does not already exist. If no
+          # argument is given, session name will be based on layout file name.
+          if initialize_session "dashboard"; then
+
+            # Create a new window inline within session layout definition.
+            new_window "notes"
+            run_cmd "nvim ~/Second_Brain/areasOfResponsibility/Ekiree/Tech"
+
+            new_window "nvim"
+            run_cmd "nvim ./ekiree_dashboard"
+
+            new_window "git"
+            run_cmd "lazygit"
+
+            new_window "term"
+            run_cmd "cd ekiree_dashboard"
+
+            # Select the default active window on session creation.
+            select_window 2
+
+          fi
+
+          # Finalize session creation and switch/attach to it.
+          finalize_and_go_to_session
+        '';
+      ".tmux-layouts/axs.session.sh".text =
+        /*
+        bash
+        */
+        ''
+          # Set a custom session root path. Default is `$HOME`.
+          # Must be called before `initialize_session`.
+          session_root "~/Documents/Github/axs-configurations"
+
+          # Create session with specified name if it does not already exist. If no
+          # argument is given, session name will be based on layout file name.
+          if initialize_session "dashboard"; then
+
+            # Create a new window inline within session layout definition.
+            new_window "notes"
+            run_cmd "nvim ~/Work_Brain"
+
+            new_window "nvim"
+            run_cmd "nvim ."
+
+            new_window "git"
+            run_cmd "lazygit"
+
+            new_window "term"
+            run_cmd "cd nixos"
+
+            # Select the default active window on session creation.
+            select_window 2
+
+          fi
+
+          # Finalize session creation and switch/attach to it.
+          finalize_and_go_to_session
+        '';
     };
   };
 }
