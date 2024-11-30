34c13ff7889b7697e99d39a00c2c00b264e20f27
Kaddu720 <nwilsonmalgus@gmail.com>
2024-11-29 21:58:37 -0800

    fix nushell welcome message

--- modules/home-manager/optional/dev-modules/nushell.nix
+++ modules/home-manager/optional/dev-modules/nushell.nix
@@ -16,9 +16,21 @@
         zoxide init nushell | save -f ~/.zoxide.nu
       '';
       configFile.text = ''
+        # set up welcome banner
+        def welcome [] {
+          print $"Praise the Omnisiah"
+        }
+        welcome
+
         #enable zoxide
         source ~/.zoxide.nu
         alias cd = z
+
+      '';
+      extraConfig = ''
+        $env.config = {
+          show_banner: false
+        }
       '';
     };
 
