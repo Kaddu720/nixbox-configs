d4f36ab9a85e9ba5bfd3a245f6299cea1e084dd4
Kaddu720 <nwilsonmalgus@gmail.com>
2024-12-03 09:18:35 -0800

    adding dashboard configuratin profile to tmuxifier

--- modules/home-manager/optional/dev-modules/tmux.nix
+++ modules/home-manager/optional/dev-modules/tmux.nix
@@ -124,42 +124,78 @@
           # Finalize session creation and switch/attach to it.
           finalize_and_go_to_session
         '';
-      ".tmux-layouts/axs.session.sh".text =
-        /*
-        bash
-        */
-        ''
-          # Set a custom session root path. Default is `$HOME`.
-          # Must be called before `initialize_session`.
-          session_root "~/Documents/Github/axs-configurations"
 
-          # Create session with specified name if it does not already exist. If no
-          # argument is given, session name will be based on layout file name.
-          if initialize_session "AXS"; then
+        ".tmux-layouts/dashConfig.session.sh".text =
+          /*
+          bash
+          */
+          ''
+            # Set a custom session root path. Default is `$HOME`.
+            # Must be called before `initialize_session`.
+            session_root "~/Projects/dashboard/configuration/"
 
-            # Create a new window inline within session layout definition.
-            new_window "notes"
-            run_cmd "/Users/noahwilson/WorkBrain"
-            run_cmd "nvim ."
+            # Create session with specified name if it does not already exist. If no
+            # argument is given, session name will be based on layout file name.
+            if initialize_session "dashboard"; then
 
-            new_window "nvim"
-            run_cmd "nvim ."
+              # Create a new window inline within session layout definition.
+              new_window "notes"
+              run_cmd "nvim ~/Second_Brain/areasOfResponsibility/Ekiree/Tech"
 
-            new_window "git"
-            run_cmd "lazygit"
+              new_window "nvim"
+              run_cmd "nvim"
 
-            new_window "term"
-            run_cmd "cd nixos"
-            run_cmd "clear"
+              new_window "git"
+              run_cmd "lazygit"
 
-            # Select the default active window on session creation.
-            select_window 1
+              new_window "term"
 
-          fi
+              # Select the default active window on session creation.
+              select_window 2
 
-          # Finalize session creation and switch/attach to it.
-          finalize_and_go_to_session
-        '';
+            fi
+
+            # Finalize session creation and switch/attach to it.
+            finalize_and_go_to_session
+          '';
+
+        ".tmux-layouts/axs.session.sh".text =
+          /*
+          bash
+          */
+          ''
+            # Set a custom session root path. Default is `$HOME`.
+            # Must be called before `initialize_session`.
+            session_root "~/Documents/Github/axs-configurations"
+
+            # Create session with specified name if it does not already exist. If no
+            # argument is given, session name will be based on layout file name.
+            if initialize_session "AXS"; then
+
+              # Create a new window inline within session layout definition.
+              new_window "notes"
+              run_cmd "/Users/noahwilson/WorkBrain"
+              run_cmd "nvim ."
+
+              new_window "nvim"
+              run_cmd "nvim ."
+
+              new_window "git"
+              run_cmd "lazygit"
+
+              new_window "term"
+              run_cmd "cd nixos"
+              run_cmd "clear"
+
+              # Select the default active window on session creation.
+              select_window 1
+
+            fi
+
+            # Finalize session creation and switch/attach to it.
+            finalize_and_go_to_session
+          '';
+      };
     };
   };
 }
