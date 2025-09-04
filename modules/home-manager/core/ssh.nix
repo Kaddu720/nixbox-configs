1c3f5c0c7e8ec33b3071ae5d22ce6337e8fe98f7
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-09-04 13:33:08 -0700

    update ssh to new format

--- modules/home-manager/core/ssh.nix
+++ modules/home-manager/core/ssh.nix
@@ -12,11 +12,14 @@
   config = lib.mkIf config.ssh.enable {
     programs.ssh = {
       enable = true;
-      addKeysToAgent = "yes";
+      enableDefaultConfig = false;
       extraOptionOverrides = {
         SetEnv = "TERM=xterm";
       };
       matchBlocks = {
+        "*" = {
+          addKeysToAgent = "yes";
+        };
         "github.com" = {
           user = "git";
           identityFile = "${vars.sshKey}";
