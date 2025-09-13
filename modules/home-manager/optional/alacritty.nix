025252fa81398b9713375797677fc717afdb7650
Kaddu720 <nwilsonmalgus@gmail.com>
2025-09-12 17:28:58 -0700

    fix alacritty configs

--- modules/home-manager/optional/alacritty.nix
+++ modules/home-manager/optional/alacritty.nix
@@ -2,35 +2,106 @@
   pkgs,
   lib,
   config,
+  vars,
   ...
-}: {
+}: let
+  font-size =
+    if ("${vars.hostName}" == "Home-Box")
+    then 12
+    else 18;
+in {
   options = {
     alacritty.enable =
       lib.mkEnableOption "enables alacritty";
   };
 
   config = lib.mkIf config.alacritty.enable {
-    programs.alacritty = {
-      enable = true;
+    home = let
+      alacritty-config = pkgs.writeText "alacritty-config" ''
+        [env]
+        TERM = "xterm-256color"
 
-      settings = {
-        terminal.shell = {
-          program = "${pkgs.fish}/bin/fish";
-        };
+        [terminal.shell]
+        program = "${pkgs.fish}/bin/fish"
+        args = []
 
-        env = {
-          term = "xterm-256color";
-        };
+        [window]
+        padding.x = 10
+        padding.y = 5
+        dynamic_padding = true
+        decorations = "None"
+        opacity = 1.0
 
-        window = {
-          padding = {
-            x = 10;
-            y = 5;
-          };
-          dynamic_padding = true;
-          decorations = "Buttonless";
-        };
-      };
+        [font]
+        size = ${toString font-size}
+
+        [font.normal]
+        family = "JetBrains Mono Nerd Font"
+        style = "Regular"
+
+        [font.bold]
+        family = "JetBrains Mono Nerd Font"
+        style = "Bold"
+
+        [font.italic]
+        family = "JetBrains Mono Nerd Font"
+        style = "Italic"
+
+        [colors.primary]
+        background = "#191724"
+        foreground = "#8c9aa5"
+
+        [colors.cursor]
+        text = "#c7c4c4"
+        cursor = "#c7c4c4"
+
+        [colors.selection]
+        text = "#191724"
+        background = "#c7c4c4"
+
+        [colors.normal]
+        black = "#191724"
+        red = "#b4637a"
+        green = "#bb5c3a"
+        yellow = "#c78645"
+        blue = "#3b83aa"
+        magenta = "#563ea9"
+        cyan = "#3b83aa"
+        white = "#c7c4c4"
+
+        [colors.bright]
+        black = "#aa7264"
+        red = "#aa7264"
+        green = "#bb5c3a"
+        yellow = "#c78645"
+        blue = "#3b83aa"
+        magenta = "#3b83aa"
+        cyan = "#563ea9"
+        white = "#c7c4c4"
+
+        [cursor]
+        style = "Block"
+        unfocused_hollow = true
+
+        [scrolling]
+        history = 10000
+        multiplier = 3
+
+        [mouse]
+        hide_when_typing = true
+
+        [keyboard]
+        bindings = [
+          { key = "V", mods = "Control|Shift", action = "Paste" },
+          { key = "C", mods = "Control|Shift", action = "Copy" },
+          { key = "Plus", mods = "Control", action = "IncreaseFontSize" },
+          { key = "Minus", mods = "Control", action = "DecreaseFontSize" },
+          { key = "Key0", mods = "Control", action = "ResetFontSize" }
+        ]
+      '';
+    in {
+      packages = with pkgs; [alacritty];
+      file.".config/alacritty/alacritty.toml".source = "${alacritty-config}";
     };
   };
 }
