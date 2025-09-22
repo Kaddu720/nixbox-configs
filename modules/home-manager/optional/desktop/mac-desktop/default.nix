e39d62000cb4e1a3694e84d66d07623c9727b665
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-09-22 16:07:16 -0700

    programatically set mac wallpaper

--- modules/home-manager/optional/desktop/mac-desktop/default.nix
+++ modules/home-manager/optional/desktop/mac-desktop/default.nix
@@ -1,13 +1,26 @@
 {
   config,
   lib,
+  pkgs,
   ...
-}: {
+}:
+let
+  wallpaper = builtins.path {
+    path = ../../../../common/static/dark_fractal.jpg;
+    name = "dark_fractal.jpg";
+  };
+in
+{
   imports = [
     ./aerospace.nix
   ];
 
   config = lib.mkIf (config.services.desktop-config.macDesktop == true) {
     aerospace.enable = true;
+    
+    # Set wallpaper
+    home.activation.setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD /usr/bin/osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"${wallpaper}\""
+    '';
   };
 }
